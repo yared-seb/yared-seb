367c55bb6dcb7b2188e7adba1ef620cc53ff7536
yared-seb <yaredaytenew21@gmail.com>
2025-09-08 05:35:42 -0700

    Delete yaberus directory

--- yaberus/course/course.js
+++
@@ -1,5 +0,0 @@
-function toggleSubjects(id) {
-  const section = document.getElementById(id);
-  const isVisible = section.style.display === "block";
-  section.style.display = isVisible ? "none" : "block";
-}
